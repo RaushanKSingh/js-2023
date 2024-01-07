49b5eb48136be0e3dd45acc185d0932dc33e71bc
RaushanKSingh <84720972+RaushanKSingh@users.noreply.github.com>
2024-01-07 13:38:39 +0000

    arrays

--- 01_basic/07_dates.js
+++ 01_basic/07_dates.js
@@ -2,5 +2,5 @@
 
 let myDate = new Date();
 console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
+console.log(myDate.toDateString);
+console.log(myDate.toLocaleTimeString());
