14ad6c8ccac829b10b258f596b9c5260f65c4311
RaushanKSingh <84720972+RaushanKSingh@users.noreply.github.com>
2024-01-07 14:29:09 +0000

    arrays

--- 02_basic/01_arrays.js
+++ 02_basic/01_arrays.js
@@ -1,4 +1,34 @@
 // arrays
 
 const myArr = [0,1,2,3,4,5];
-console.log(myArr);
+const myHeros =["shaktiman","nagraj"];
+const myArr2 = new Array(1,2,3,4);
+
+// console.log(myArr);
+// console.log(myArr[1]);
+
+// myArr.push(6);
+// myArr.push(7);
+// console.log(myArr);
+
+// myArr.pop();
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(4));
+// console.log(myArr.includes(14));
+// console.log(myArr.indexOf(4));
+
+// console.log(myArr);
+
+// slice and splice
+// console.log("original Array myArr: ",myArr,"\n");
+
+// let nArr1 = myArr.slice(1,4);
+// console.log("sliced array: ",nArr1);
+// console.log("After slice, myArr: ",myArr,"\n");
+
+// let nArr2 = myArr.splice(1,3);
+// console.log("spliced array: ",nArr2);
+// console.log("After splice, myArr: ",myArr);
+
---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,21 @@
+const marvel_heros = ["thor", "ironman","spiderman"];
+const dc_heros = ["superman","flash","batman"];
+
+// marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros[3][1]);
+// let n_heros=marvel_heros.concat(dc_heros);
+
+// console.log(n_heros);
+
+// const all_heros = [...marvel_heros,...dc_heros];
+// console.log(all_heros);
+
+console.log(Array.isArray("raushan"));
+console.log(Array.from("raushan"));
+
+let s1= 25;
+let s2= 56;
+let s3= 69;
+
+console.log(Array.of(s1,s2,s3));
