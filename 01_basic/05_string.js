4dfb38f17cd822c3b1492c3c9a8baa4c12c6f5c0
RaushanKSingh <84720972+RaushanKSingh@users.noreply.github.com>
2024-01-07 09:34:57 +0000

    string update 1

---
+++ 01_basic/05_string.js
@@ -0,0 +1,10 @@
+
+const name = "loveShivani"
+console.log(name.toUpperCase());
+console.log(name.indexOf("v"));
+console.log(name.charAt(3));
+
+const newString = name.substring(2,7);
+console.log(newString);
+console.log(name.slice(-8,8));
+
