06d781a86cce4562294278b5dd045a02566f7098
RaushanKSingh <84720972+RaushanKSingh@users.noreply.github.com>
2024-01-09 14:28:45 +0000

    object1

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,35 @@
+// object singleton
+
+//object 
+
+const sym =Symbol("sym1");
+// console.log(sym);
+// console.log(typeof sym);
+const myObj = {
+    name:"raushan",
+    age:28,
+    email:"raushan@google.com",
+    "full_name":"Raushan Kumar",
+    [sym]:"love",
+};
+
+// console.log(myObj.name);
+// console.log(myObj.age);
+// console.log(myObj["full_name"]);
+// console.log(typeof myObj[sym]);
+// console.log(myObj[sym]);
+// console.log(myObj);
+// console.log(typeof myObj[sym]);
+
+
+myObj.email ="hotstar@gmaill.com";
+// Object.freeze(myObj);
+// myObj.email ="great@gmaill.com"; // no change after object freeze 
+// console.log(myObj);
+
+myObj.greetings = function(){
+    console.log("hello myObj")
+};
+// console.log(myObj)
+console.log(myObj.greetings);
+console.log(myObj.greetings());
