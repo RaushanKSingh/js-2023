54571033abc4c531edf5fd88a4b6fa6f32a5907d
RaushanKSingh <84720972+RaushanKSingh@users.noreply.github.com>
2024-01-11 17:20:20 +0000

    arrowfn

---
+++ 03_basic/03_arrowfn.js
@@ -0,0 +1,24 @@
+function one (){
+    const user= hitesh;
+}
+
+// const user ={
+//     userName:"raushan",
+//     userCode: "143",
+//     userFn:function(){
+//         console.log(`user name is ${this.userName}`);
+//         console.log(this);
+//     }
+// }
+
+// user.userFn();
+// console.log(this);
+// console.log(user.userCode);
+
+// function one (){
+//     // console.log(this);
+//     const user= 'hitesh';
+//     console.log(this);
+
+// }
+// one();
