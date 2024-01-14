04611d6cfd2a1b4ba50be5015bbc503039350f51
RaushanKSingh <84720972+RaushanKSingh@users.noreply.github.com>
2024-01-14 05:42:02 +0000

    control flow

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,6 @@
+// ( function sum (x,y) {
+//      console.log(x+y);
+//  })(2,6);
+
+
+//  ((name)=> console.log(`my name is ${name}`))("raushan");
---
+++ 04_controlflow/one.js
@@ -0,0 +1,14 @@
+const score = 1000;
+
+if (score<500){
+    console.log("less than 500");
+}else if  (score<700){
+    console.log("less than 700");
+}else if(score <900){
+    console.log("less than 900");
+}else {
+    console.log("greater than 900");
+
+}
+
+if(true) console.log("single line scope without curly braces.");
