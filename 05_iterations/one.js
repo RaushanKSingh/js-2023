7b272939739fd036d201c3609f1f6cdc95a2e805
RaushanKSingh <84720972+RaushanKSingh@users.noreply.github.com>
2024-01-14 11:54:25 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,45 @@
+let arr =[1,2,3,4,5,6,7,8,9,10]
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(`element at index ${index} is ${element}`);
+    
+// }
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     if (element==5) {
+//         console.log(`best number ${element}`);
+//         break;
+//     }
+//     console.log(`element at index ${index} is ${element}`);
+    
+// }
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     if (element==5) {
+//         console.log(`best number ${element}`);
+//         continue;
+//     }
+//     console.log(`element at index ${index} is ${element}`);
+    
+// }
+
+// let index = 0;
+// while (index< arr.length) {
+   
+//     const element = arr[index];
+//     console.log(`element at index ${index} is ${element}`)
+//     index++;
+// }
+// let index = 0;
+// while (arr[index]) {
+    
+//     const element = arr[index];
+//     console.log(`element at index ${index} is ${element}`)
+//     index++;
+// }
+// let index = 0;
+// do {
+//     const element = arr[index];
+//     console.log(`element at index ${index} is ${element}`);
+//     index++;
+} while (index< arr.length);
